837b90c51839ea84fc561596a981e9b8bc95f974
LefevreGregoire <lefevregregoire@proton.me>
2026-09-25 09:22:41 +0200

    test: simulate real brute force in attaque.py

--- py/attaque.py
+++ py/attaque.py
@@ -1,29 +1,39 @@
 import time
 import json
 import random
+import os
 import paho.mqtt.client as mqtt
+import cypher
+
+# Définition de la clé pour chiffrer
+os.environ['IDS_SECRET_KEY'] = 'JKp-xlgYazhuZZRf2R1c6_xj-6Jhz2kYXwEn8ydy5zc='
 
 BROKER = "192.168.50.171"
 PORT = 1883
 TOPIC = "cyberspace/capteurs/salle-serveur"
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "HackedSensor_Debian")
+client.username_pw_set("v-client", "secret_iot_2026")
 client.connect(BROKER, PORT)
 
-print("Attack test")
+print("🚀 Démarrage de l'attaque Brute Force (Auth Failure Spam)...")
 
 try:
     while True:
         payload = {
-            "temp": random.randint(150, 999), 
-            "status": "COMPROMISED"
+            "type": "AUTH_FAILURE",
+            "device": "sas",
+            "ip": "192.168.50.42",
+            "message": "Invalid credentials",
+            "temp": random.randint(150, 999)
         }
         
-        client.publish(TOPIC, json.dumps(payload))
-        print(f"Spam: {payload}")
+        # Envoi d'une fausse alerte chiffrée pour bypasser la passerelle
+        client.publish(TOPIC, cypher.encrypt_message("sas", payload))
+        print(f"💥 Spam Brute Force : {payload}")
         
         time.sleep(0.1)
         
 except KeyboardInterrupt:
-    print("\nAttaque interrompue par l'utilisateur.")
+    print("\n🛑 Attaque interrompue par l'utilisateur.")
     client.disconnect()
